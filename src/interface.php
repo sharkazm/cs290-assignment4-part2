bc1c4d2c58b5595a92277f1aec11cafa54696e01
Ashton Herrington <ashtonherrington.ah@gmail.com>
2015-05-06 20:21:53 -0500

    revamped project as I was beginning incorrectly with JS, set up form in HTML with PHP backend

--- src/interface.php
+++ src/interface.php
@@ -1,13 +1,5 @@
 <?php
 ini_set('display_errors', 1);
-echo '<html>
-<head>
-<title>Content 1 PHP</title>
-<script src="assignment3.js"></script>
-</head>
-<body>';
-
-echo "Test test";
 
 $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "herrinas-db", "3JCPnCFTmsZs8ASZ", "herrinas-db");
 if(!$mysqli || $mysqli->connect_errno){
@@ -15,13 +7,4 @@ if(!$mysqli || $mysqli->connect_errno){
 }
 
 
-echo "<form>
-<input type='button' value='Add new video' onclick='input_video()' />
-</form>";
-
-echo '<div id="dynamicSection">I should see more here: </div>';
-
-
-echo '</body>
-</html>'; 
 ?>
